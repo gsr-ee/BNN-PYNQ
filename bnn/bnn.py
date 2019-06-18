a02a29de26fb7fd6915a4084f7e0b4246eeb2e95
Gabriel Silva Rocha <gsr.ee@hotmail.com>
2019-06-17 23:10:13 -0300

    Update bnn.py

--- bnn/bnn.py
+++ bnn/bnn.py
@@ -48,7 +48,7 @@ if os.environ['BOARD'] == 'Ultra96':
 elif os.environ['BOARD'] == 'Pynq-Z1' or os.environ['BOARD'] == 'Pynq-Z2':
 	PLATFORM="pynqZ1-Z2"
 elif os.environ['BOARD'] == 'Zedboard':
-	PLATFORM="zedboard"
+	PLATFORM="pynqZ1-Z2"
 else:
 	raise RuntimeError("Board not supported")
 
